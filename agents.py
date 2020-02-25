cd2325f75b195d125a75faf585334091caccbcf5
Ptomasetta@umass.edu <ptomasrtta@umass.edu>
2020-02-25 16:03:22 -0500

    Implemented non-depth restrictive minimax, edited starter code to run properly, added a fuller test board

--- agents.py
+++ agents.py
@@ -5,7 +5,7 @@ import math
 class RandomAgent:
     """Agent that picks a random available move.  You should be able to beat it."""
     def get_move(self, state, depth=None):
-        possibles = list(state.successors().items())
+        possibles = list(state.successors())
         return random.choice(possibles)
 
 
@@ -13,9 +13,13 @@ class HumanAgent:
     """Prompts user to supply a valid move."""
     def get_move(self, state, depth=None):
         move__state = state.successors()
-        prompt = "Kindly enter your move {}: ".format(sorted(move__state.keys()))
+        moves = {}
+        for tup in move__state:
+            moves[tup[0]] = tup[1]
+        
+        prompt = "Kindly enter your move {}: ".format(sorted([move[0] for move in move__state]))
         move = int(input(prompt))
-        return move, move__state[move]
+        return move, moves[move]
 
 
 class ComputerAgent:
@@ -29,7 +33,7 @@ class ComputerAgent:
         best_state = None
 
         for move, state in state.successors():
-            util = self.minimax(state, depth)
+            util = self.minimax(state, depth) 
             if ((nextp == 1) and (util > best_util)) or ((nextp == -1) and (util < best_util)):
                 best_util, best_move, best_state = util, move, state
         return best_move, best_state
@@ -46,9 +50,31 @@ class ComputerAgent:
 
         Returns: the minimax utility value of the state
         """
-        #
-        # Fill this in!
-        #
+
+        #have to deal with depth for when not None?
+
+        win = state.winner() 
+        nextp = state.next_player() # whos turn it is 
+
+        if win is not None: # reaches a terminal state
+            print("Terminl state detected: ", win)
+            return win # 1 if Player 1 wins, -1 if Player 2 wins, 0 if the board is full (indicating a tie)
+        elif win == 1: # MAX is to move in state
+            minimax_value = -2
+            for move, child in state.successors():
+                util = self.minimax(child, depth) 
+                minimax_value = max(minimax_value, util)
+            return minimax_value
+        else: # MIN is to move in state
+            minimax_value = 2
+            for move, child in state.successors():
+                util = self.minimax(child, depth) 
+                minimax_value = min(minimax_value, util)
+            return minimax_value
+
+ 
+
+
         return 42  # Change this line!
 
     def evaluation(self, state): #zzz must be O(1)
--- connect4.py
+++ connect4.py
@@ -169,7 +169,10 @@ if __name__ == "__main__":
     play2 = agent_codes[sys.argv[2]]()
     depth_lim = None if len(sys.argv) < 4 else int(sys.argv[3])  # optional argument
 
-    start_state = None  # change this to tests.board1 to use a test state instead of a blank board
+    test_board = GameState()
+    test_board.board = tests.board4 # added this to get tests to work
+    
+    start_state = test_board  # change this to tests.board1 to use a test state instead of a blank board
 
     play_game(play1, play2, depth_lim, start_state)
 
